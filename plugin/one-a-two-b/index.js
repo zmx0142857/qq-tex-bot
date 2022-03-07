de817433228e52bed2cddd2c0f48d3e8795ba2e4
zmx0142857 <892298182@qq.com>
2022-03-07 17:40:20 +0800

    1a2b rank 持久化 (JSON)

--- plugin/one-a-two-b/index.js
+++ plugin/one-a-two-b/index.js
@@ -1,4 +1,5 @@
-const message = require("../../message");
+const message = require('../../message')
+const { loadRank, saveRank } = require('./rank')
 
 const store = {} // { groupId: Game }
 const defaultLen = 4
@@ -71,20 +72,25 @@ function judge(current, guess) {
   return a === current.len
 }
 
-module.exports = async function oneATwoB(text, sender, chain) {
+module.exports = async function oneATwoB (text, sender, chain) {
   const groupId = sender.group && sender.group.id
+  console.log(sender, chain)
   console.log('groupId', groupId)
   if (text === '') {
     return [{
       type: 'Plain',
       text: `用法:
 /1a2b new   新的游戏
+/1a2b rank  查看排行
 /1a2b <${defaultLen} 位不同数字>   参与游戏
 `
     }]
   } else if (text === 'new') {
     newGame(groupId)
     return message.plain(`已生成新的 ${store[groupId].len} 位数字`)
+  } else if (text === 'rank') {
+    const rank = await loadRank(groupId)
+    return message.plain(rank)
   } else if (/\d+/.test(text)) {
     const current = store[groupId]
     if (!current) {
@@ -103,6 +109,11 @@ module.exports = async function oneATwoB(text, sender, chain) {
     const prompt = isWin ? `恭喜你猜对了！ ${current.guessCount}/${current.limit}`
       : isLose ? `已猜 ${current.limit} 次，游戏结束。\n答案：${current.answer}`
       : `输入 /1a2b <${current.len} 位不同数字> 参与游戏`
+
+    // 记录成绩
+    if (isWin) {
+      saveRank(groupId, sender)
+    }
 
     return message.plain(prompt + '\n' + current.history.join('\n'))
   }
---
+++ plugin/one-a-two-b/rank.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+
+function readFileData (groupId) {
+  const filename = `1a2b.${groupId}.json`
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        if (err.code === 'ENOENT')
+          data = [] // 不存在就新建
+        else
+          return reject(err)
+      } else {
+        data = JSON.parse(data)
+      }
+      resolve(data)
+    }) 
+  })
+}
+
+async function loadRank (groupId) {
+  const data = await readFileData(groupId)
+  if (!data.length) return '本群暂无成绩'
+  return data.map(d => `${d.name} ${d.score}`).join('\n')
+}
+
+async function saveRank (groupId, sender) {
+  const filename = `1a2b.${groupId}.json`
+  const data = await readFileData(groupId)
+  const record = data.find(d => d.id === sender.id)
+  if (record) {
+    record.score += 1
+  } else {
+    data.push({id: sender.id, name: sender.memberName || sender.name, score: 0})
+  }
+  fs.writeFileSync(filename, JSON.stringify(data))
+}
+
+module.exports = {
+  loadRank,
+  saveRank,
+}
