1263f47b4ad5d4dd295622333b7af5158cacf826
zmx0142857 <892298182@qq.com>
2022-11-23 17:56:14 +0800

    add riddle/help.js

--- src/plugin/riddle/index.js
+++ src/plugin/riddle/index.js
@@ -2,20 +2,10 @@ const message = require('../../message')
 const { loadRank, saveRank, loadScore, saveScore, clearScore } = require('./rank')
 const { riddleGroup } = require('../../config').auth
 const { getRiddle, resetRiddle, putBackRiddle } = require('./source')
+const helpDict = require('./help')
 
 const store = {} // { groupId: { question, answer, raw, timer1, timer2 } }
-const helpDict = {
-  '露春': '谜面不露春，是指谜面上不会出现谜底的任何一个字。谜目则无此限制，可以出现谜底中的字',
-  '探骊格': '探骊格如同深海探宝，不指定谜目，要求将谜目谜底一起猜出，谜目谜底连起来与谜面相扣',
-  '卷帘格': '卷帘格，意为『倒卷珠帘』，要把谜底倒过来读，如谜底是『孙行者』，则要读作『者行孙』',
-  '离合字': '离合字是将一个字拆成多个部件，如：好女子、弓长张',
-  '空格': '谜底为多个组合时用空格隔开，如：中秋 端午',
-  '梨花格': '梨花格是谐音格，可以放心使用谐音梗',
-  '徐妃格': '取『只得徐妃半面妆』之意，去掉谜底相同的偏旁',
-  '摘匾格': '类似徐妃格，去掉谜底相同的部首',
-  '白头格': '首字谐音',
-  '折巾格': '将谜底第一字左右拆开成两个字，只取其半边字连下文来读',
-}
+
 let skipIsBusy
 
 function invalidateRiddle (groupId) {
@@ -71,7 +61,7 @@ async function riddle (text, sender, chain) {
     const page = parseInt(text.slice(5)) || 1
     const rank = await loadRank(groupId, page)
     return message.plain(rank)
-  } else if (text === 'begin') {
+  } else if (text === 'remake') {
     clearScore(groupId)
     resetRiddle(groupId)
     return message.plain('游戏已就绪，发送 /riddle get 查看谜面')
@@ -102,7 +92,7 @@ async function riddle (text, sender, chain) {
     if (help) return message.plain(help)
     return message.plain(`猜灯谜。用法：
 /riddle get 查看谜面
-/riddle begin 开始计分
+/riddle remake 重新开始游戏
 /riddle score [页码] 查看计分
 /riddle rank [页码] 查看总排行
 /riddle hint 获取提示
