f7d8c08e2976eb283f3d38528f7025cd5a4c096c
zmx0142857 <892298182@qq.com>
2022-12-09 18:00:37 +0800

    fix riddle

--- src/plugin/riddle/source.js
+++ src/plugin/riddle/source.js
@@ -10,34 +10,38 @@ function syncData(groupId, lines) {
   writeJson(filename, data)
 }
 
-async function getRiddle (groupId) {
-  let text, lines
-  if (store[groupId]) {
-    lines = store[groupId]
-  } else {
-    try {
-      text = await fs.promises.readFile('data/riddle.txt', 'utf-8')
-    } catch (e) {
-      console.error(e)
-      return { code: 1, message: '获取谜面失败，请稍后再试' }
-    }
-    if (!text.trim()) lines = []
-    else lines = text.trim().split('\n').sort(() => Math.random() < 0.5 ? -1 : 1)
-    store[groupId] = lines
-    syncData(groupId, lines)
+async function initStore (groupId) {
+  let text
+  let lines = store[groupId]
+  if (lines) return lines
+  try {
+    text = await fs.promises.readFile('data/riddle.txt', 'utf-8')
+  } catch (e) {
+    console.error(e)
+    return { code: 1, message: '获取谜面失败，请稍后再试' }
   }
+  if (!text.trim()) lines = []
+  else lines = text.trim().split('\n').sort(() => Math.random() < 0.5 ? -1 : 1)
+  store[groupId] = lines
+  syncData(groupId, lines)
+  return lines
+}
+
+async function getRiddle (groupId) {
+  const lines = await initStore(groupId)
   if (!lines.length) return { code: 2, message: '已经没有更多谜题了！' }
   const randLine = lines.pop()
   try {
     const [face, category, answer, hint] = randLine.split(',')
-    syncData(groupId, lines)
-    return {
+    const ret = {
       code: 0,
       question: `${face}【${category}】`,
       answer: answer.trim(),
       raw: randLine,
       hint: hint || '没有提示捏',
     }
+    syncData(groupId, lines)
+    return ret
   } catch (e) {
     console.error(e)
     console.error('randLine:', randLine)
